ec8552e3cb428f03a423506655998670328d3c2e
Sérgio Matheus <sergiomatheuss09@gmail.com>
2024-01-02 20:56:49 -0300

    Clippy completed. Half conversions's exercises done.

--- exercises/conversions/from_into.rs
+++ exercises/conversions/from_into.rs
@@ -7,6 +7,8 @@
 // Execute `rustlings hint from_into` or use the `hint` watch subcommand for a
 // hint.
 
+use std::result;
+
 #[derive(Debug)]
 struct Person {
     name: String,
@@ -40,10 +42,23 @@ impl Default for Person {
 // If while parsing the age, something goes wrong, then return the default of
 // Person Otherwise, then return an instantiated Person object with the results
 
-// I AM NOT DONE
-
 impl From<&str> for Person {
     fn from(s: &str) -> Person {
+        let (name, age) = match s.split(',').collect::<Vec<&str>>()[..] {
+            [name, age, ..] => (name.trim(), age.trim()),
+            _ => return Person::default(),
+        };
+
+        if let Ok(age) = age.parse::<usize>() {
+            if name.len() > 0 {
+                return Person {
+                    name: String::from(name),
+                    age,
+                };
+            }
+        }
+
+        Person::default()
     }
 }
 
--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -41,6 +41,29 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        match tuple {
+            (a, _, _) => {
+                if a.is_negative() || a > 255 {
+                    return Err(IntoColorError::IntConversion);
+                }
+            }
+            (_, b, _) => {
+                if b.is_negative() || b > 255 {
+                    return Err(IntoColorError::IntConversion);
+                }
+            }
+
+            (_, _, c) => {
+                if c.is_negative() || c > 255 {
+                    return Err(IntoColorError::IntConversion);
+                }
+            }
+        }
+        Ok(Color {
+            red: tuple.0 as u8,
+            green: tuple.1 as u8,
+            blue: tuple.2 as u8,
+        })
     }
 }
 
@@ -48,6 +71,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: arr[0] as u8,
+            green: arr[1] as u8,
+            blue: arr[2] as u8,
+        })
     }
 }
 
@@ -55,6 +83,11 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        Ok(Color {
+            red: slice[0] as u8,
+            green: slice[1] as u8,
+            blue: slice[2] as u8,
+        })
     }
 }
 
