ca78e062628b29c568f73323f1af23958617a808
Sérgio Matheus <sergiomatheuss09@gmail.com>
2024-01-04 20:20:33 -0300

    Finished rustlings ;D

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -27,8 +27,6 @@ enum IntoColorError {
     IntConversion,
 }
 
-// I AM NOT DONE
-
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
 // integers, an array of three integers, and a slice of integers.
@@ -41,22 +39,10 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
-        match tuple {
-            (a, _, _) => {
-                if a.is_negative() || a > 255 {
-                    return Err(IntoColorError::IntConversion);
-                }
-            }
-            (_, b, _) => {
-                if b.is_negative() || b > 255 {
-                    return Err(IntoColorError::IntConversion);
-                }
-            }
-
-            (_, _, c) => {
-                if c.is_negative() || c > 255 {
-                    return Err(IntoColorError::IntConversion);
-                }
+        let (red, green, blue) = tuple;
+        for color in [red, green, blue] {
+            if !(0..=255).contains(&color) {
+                return Err(IntoColorError::IntConversion);
             }
         }
         Ok(Color {
@@ -71,6 +57,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        for color in arr {
+            if !(0..=255).contains(&color) {
+                return Err(IntoColorError::IntConversion);
+            }
+        }
         Ok(Color {
             red: arr[0] as u8,
             green: arr[1] as u8,
@@ -83,6 +74,15 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3 {
+            return Err(IntoColorError::BadLen);
+        }
+        for color in slice {
+            if !(0..=255).contains(color) {
+                return Err(IntoColorError::IntConversion);
+            }
+        }
+
         Ok(Color {
             red: slice[0] as u8,
             green: slice[1] as u8,
